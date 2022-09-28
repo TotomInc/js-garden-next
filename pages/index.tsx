ad4c4286208b2f8bcba5485314870f57a68ab48c
Thomas Cazade <cazade.thomas@gmail.com>
2022-09-28 23:24:55 +0200

    Remove dead-code from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,13 @@
-import type { NextPage, GetStaticProps } from "next";
+import type { NextPage } from "next";
 
-import type { Post } from "../interfaces/posts.interfaces";
-import { getAllPosts } from "../lib/api";
 import { DefaultLayout } from "../components/DefaultLayout";
 import { SEO } from "../components/SEO";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
-
-  return { props: { posts: allPosts } };
-};
-
-const Home: NextPage<{ posts: Post[] }> = ({ posts }) => {
-  console.log("[index] posts:", posts);
-
-  return (
-    <>
-      <SEO />
-      <DefaultLayout>{null}</DefaultLayout>
-    </>
-  );
-};
+const Home: NextPage = () => (
+  <>
+    <SEO />
+    <DefaultLayout>{null}</DefaultLayout>
+  </>
+);
 
 export default Home;
