78f34ec04db7c6bde20c8a69821801c0010ffd3b
Thomas Cazade <cazade.thomas@gmail.com>
2022-09-28 11:36:58 +0200

    Add custom scrollbars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import "../styles/index.css";
 import "../styles/fonts.css";
 import "../styles/syntax.css";
+import "../styles/scrollbar.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
