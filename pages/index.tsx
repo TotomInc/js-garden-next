811c0a0bc9b470b95d858f0f7f34bad2de3c14c1
Thomas Cazade <cazade.thomas@gmail.com>
2022-09-29 15:08:58 +0200

    Filter posts by tag, add featured tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage, GetStaticProps } from "next";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 import type { Post } from "../interfaces/posts.interfaces";
@@ -22,11 +22,27 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const PostsPage: NextPage<{ posts: Post[] }> = ({ posts }) => {
+  const featuredTags = ["#react", "#vue", "#typescript", "#electron"];
+
+  const inputEl = useRef<HTMLInputElement | null>(null);
+
   const [searchValue, setSearchValue] = useState("");
 
-  const filteredBlogPosts = posts.filter((post) =>
-    post.title.toLowerCase().includes(searchValue.toLowerCase())
-  );
+  const filteredBlogPosts = posts.filter((post) => {
+    if (searchValue.startsWith("#")) {
+      const searchTagValue = searchValue.slice(1);
+
+      if (inputEl.current) {
+        inputEl.current.value = searchValue;
+      }
+
+      return post.tags
+        .map((tag) => tag.toLowerCase())
+        .includes(searchTagValue.toLowerCase());
+    }
+
+    return post.title.toLowerCase().includes(searchValue.toLowerCase());
+  });
 
   return (
     <>
@@ -44,11 +60,26 @@ const PostsPage: NextPage<{ posts: Post[] }> = ({ posts }) => {
           I&apos;ve been writing online about modern front-end development
           especially about React, Vue, TypeScript and Electron. In total,
           I&apos;ve written {posts.length} articles. Use the search filter below
-          to filter by title.
+          to filter by title or tag (using the{" "}
+          <span className="font-mono text-accent">#</span> prefix).
         </p>
+
+        <div className="mb-2 flex space-x-2">
+          {featuredTags.map((featuredTag) => (
+            <button
+              key={featuredTag}
+              type="button"
+              className="font-mono text-sm uppercase text-accent"
+              onClick={() => setSearchValue(featuredTag)}
+            >
+              {featuredTag}
+            </button>
+          ))}
+        </div>
 
         <div className="relative mb-8 w-full">
           <input
+            ref={inputEl}
             aria-placeholder="Search articles"
             type="text"
             className="w-full rounded-md bg-code-background px-4 py-3 text-text ring-white ring-opacity-50 transition focus:outline-none focus:ring-2"
