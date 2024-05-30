2db564d213cf867c9fcd4d73e861509ea2cf0eab
Thomas Cazade <cazade.thomas@gmail.com>
2024-05-30 08:44:06 +0200

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/index.css";
 import "../styles/fonts.css";
@@ -7,7 +8,10 @@ import "../styles/scrollbar.css";
 
 export function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
 }
 
